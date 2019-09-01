af305eb1892610fa29847e7df91858f9d4375ea8
kenmugy <kenmugy@gmail.com>
2019-09-02 00:34:18 +0300

    added a post route

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const courses = [
 ];
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('yeaaaaa');
@@ -34,6 +36,14 @@ app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(course => course.id === parseInt(req.params.id));
   if (course) return res.json(course);
   res.status(404).json({ error: 'no book by that id' });
+});
+
+app.post('/api/courses', (req, res) => {
+  const course = {};
+  course.id = courses.length + 1;
+  course.name = req.body.name;
+  debug(course);
+  res.status(201).json(course);
 });
 
 app.listen(port, () => debug(`listening on port ${chalk.green(port)}`));
